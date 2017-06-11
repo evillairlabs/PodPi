0b3d2aad40088dacc1ee2fef72061d2a751cb8d6
Unknown <evillairlabs@gmail.com>
2017-06-11 17:32:31 -0500

    Optimize blink
    create const vars for port, blink interval

--- Book00/blink.js
+++ Book00/blink.js
@@ -1,8 +1,12 @@
 const five = require('johnny-five');
 
+const PIN_LED1  = 3;
+
+const BLINK_INTERVAL_LED1   = 500;
+
 let board = new five.Board();
 board.on("ready", function () {
-    let led = new five.Led(3);
-    led.blink(500);
+    let led = new five.Led(PIN_LED1);
+    led.blink(BLINK_INTERVAL_LED1);
 });
 
